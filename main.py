24a2cdb7b4aa1616c2a6c0657a50a5964569bf49
abdeenalriyah22-png <abdeenalriyah22@gmail.com>
2026-04-18 20:40:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ except:
     pass
 
 # 3. واجهة التطبيق
-st.markdown("<h1>📄 محول ملفات PDF/EXCEL</h1>", unsafe_allow_html=True)
+st.markdown("<h1>📄 محول ملفات PDF الى EXCEL</h1>", unsafe_allow_html=True)
 st.markdown("<p>أهلاً بك يا أستاذ عابدين. ارفع ملفك الآن في المستطيل البرتقالي أدناه.</p>", unsafe_allow_html=True)
 
 uploaded_file = st.file_uploader("اختر ملف PDF", type=["pdf"])
